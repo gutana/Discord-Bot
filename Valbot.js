124fa17a4417fe889188dc294c822fc479729488
gutana <albayatm@outlook.com>
2022-06-30 03:56:07 -0600

    First working version, no real commands. Can talk to riot api now

--- Valbot.js
+++ Valbot.js
@@ -1,103 +1,48 @@
-class Bot {
-    constructor(client) {
-           this.version = "Discord Bot Version 0.0.1";
-           this.lastUpdated = "Last updated October 28, 2018";
+const fs = require('fs');
 
-           this.lolurlBaseString = "https://na1.api.riotgames.com/"
-           this.lolbyIdString = "lol/league/v3/positions/by-summoner/"
-           this.lolByNameString = "lol/summoner/v3/summoners/by-name/"
-           this.lolKeyQString = "?api_key="
-           this.lolreg = new RegExp('^[0-9\\p{L} _\\.]+$')
-           this.lolapi = "";
+class Bot 
+{    
+    constructor() {
+        this.version = "0.0.1";
+        this.lastUpdated = "June 30, 2022";
+        this.getValContent();
+    };   
 
-           client.user.setActivity("Zoe", { type: "PLAYING" })
-
-            //logs servers
-            console.log("\nConnection to Discord successful.\nConnected as " + client.user.tag + " to:\n");
-            client.guilds.forEach((guild) => {
-                console.log("Server: " + guild.name)
-                guild.channels.forEach((channel) => {
-                    console.log(` - ${channel.name} ${channel.type} ${channel.id}`)
-        })   
-    })
+    getDiscKey() {
+        this.discAPIKey = fs.readFileSync('keys/disc.key', 'utf-8', (err, data) => {
+            if (err) {
+                console.error(err);        
+            } else {
+                this.discAPIKey = data;
+                console.log("Discord API key read: " + this.discAPIKey);
+            };
+        });
+        return this.discAPIKey;
     }
-    
-    async getIdWithSumName(sumName) {
-        let getIDUrl = this.lolurlBaseString + this.lolByNameString + sumName + this.lolKeyQString + this.lolapi;
 
-        const getId = await fetch(getIDUrl)
-            .catch(err => console.error(err))
-        const resp = await getId.json()
-            .catch(err => console.error(err))
+    getValKey() {
+        this.valAPIKey = fs.readFileSync('keys/val.key', 'utf-8', (err, data) => {
+            if (err) {
+                console.error(err);
+            } else {
+                this.valAPIKey = data;
+                console.log("Valorant API key read: " + this.valAPIKey);
+            };
+        });
+        return this.valAPIKey;
+    }
 
-        console.log(await resp); 
-        const id = await resp["id"]
-        console.log("Found ID: " + id)
-        return id; 
+    async getValContent() {
+        var headers = {
+                "X-Riot-Token": this.getValKey()
+        };
+        this.valContent = await fetch("https://na.api.riotgames.com/val/content/v1/contents", {method: 'GET', headers: headers});
     }
 
     helpCommand(msg) {
-        msg.channel.send(msg.author + " I am a bot. Currently, my only commands are: \n!help - returns commands and bot info\n!rank <user> - returns all ranked queue standings including promos\n!version - returns bot version\n\nIf you have any questions, or would like to request a feature, contact my master @Gutana.")
-        msg.react("😂")
-    }
-
-    async getRank(sum, msg) {
-        let getRankUrl = "";
-
-        if (sum == "me") {
-            // if (msg.author == gutDisc) {
-            //     getRankUrl = this.lolurlBaseString + this.lolbyIdString + this.cigId + this.lolKeyQString + this.lolapi;
-            //     console.log("TRYING " + getRankUrl); 
-            // }
-            // if (msg.author == sethDisc) {
-            //     getRankUrl = this.lolurlBaseString + this.lolbyIdString + this.sethID + this.lolKeyQString + this.lolapi;
-            // }
-        } else {
-            getRankUrl = this.lolurlBaseString + this.lolbyIdString + await this.getIdWithSumName(sum) + this.lolKeyQString + this.lolapi;
-        }
-
-
-        const getId = await fetch(getRankUrl)
-            .catch(err => console.error(err))
-        const resp = await getId.json()
-            .catch(err => console.error(err))
-    
-    
-        if (resp[0]) {
-            let rank = resp[0]["playerOrTeamName"];
-            rank += " is in:\n";
-    
-            for (let i = 0; i < resp.length; i++) {
-                switch (resp[i]["queueType"]) {
-                    case "RANKED_FLEX_TT":
-                        rank += "3v3: "
-                        break; 
-                    case "RANKED_SOLO_5x5":
-                        rank += "SOLO: "
-                        break;
-                    case "RANKED_FLEX_SR":
-                        rank += "FLEX: "
-                        break;
-    
-                }
-    
-                rank += resp[i]["tier"]
-                rank += " "
-                rank += resp[i]["rank"]
-                rank += " "
-                rank += resp[i]["leaguePoints"]
-                
-                rank += " LP - "
-                rank += `${resp[i]["wins"]} wins / ${resp[i]["losses"]} losses `
-                if (resp[i]["miniSeries"]) {
-                    rank += `(${resp[i]["miniSeries"]["wins"]} - ${resp[i]["miniSeries"]["losses"]} in promos)`
-                }
-                rank += "\n";
-            }
-    
-            return await rank;
-        }
-    }
+        msg.channel.send("I am a bot. Currently, my only commands are: \n!help - returns commands and bot info\n!rank <user> - returns all ranked queue standings including promos\n!version - returns bot version\n\nIf you have any questions, or would like to request a feature, contact my master @gutana.")
+        msg.react("😂");
+    };
 
     async rankCommand(msg, args) {
         let rank;
@@ -134,15 +79,15 @@ class Bot {
                 console.error(`Error finding rank for ${sumName}`)
             }
         }, 5000);
-    }
+    };
 
     versionCommand(msg) {
         if (msg) {
-            msg.channel.send(this.version + "\n" + this.lastUpdated)
+            msg.channel.send("Val Bot version: " + this.version + "\n" + "Last updated: " + this.lastUpdated)
         } else {
             msg.channel.send("Unexpected error.")
         }
-    }
+    };
 
     processCommand(msg) {
         let fullCommand = msg.content.substr(1)
@@ -162,30 +107,29 @@ class Bot {
                 this.versionCommand(msg)
                 break
             default:
-                msg.channel.send("I'm not sure what you're trying to say, " + msg.author + 
+                msg.channel.send("I'm not sure what you're trying to say, " + msg.author.username + 
                     "\nFor a full list of commands, type '!help'.") 
         }
-    }
+    };
 }
 
 const Discord = require('discord.js');
-const client = new Discord.Client(); 
-const fetch = require('node-fetch')
+const client = new Discord.Client({ intents: ['DIRECT_MESSAGES', 'DIRECT_MESSAGE_REACTIONS', 'GUILD_MESSAGES', 'GUILD_MESSAGE_REACTIONS', 'GUILDS']}); 
+const fetch = require('node-fetch');
+const { get } = require('http');
 
-const discapi = "E";
-
-let cancerBot;
+let valBot = new Bot();
 
 client.on('ready', () => {
-    cancerBot = new Bot(client); 
-})
+    console.log("Logged in as " + client.user.tag);
+});
 
-client.on('message', (msg) => {
-    if (msg.author == client.user) { return }
+client.login(valBot.getDiscKey()); 
+
+client.on('messageCreate', (msg) => {
+    if (msg.author == client.user) { return; }
 
     if (msg.content.startsWith("!")) {
-        cancerBot.processCommand(msg); 
+        valBot.processCommand(msg); 
     }
-})
-
-client.login(discapi); 
+});
